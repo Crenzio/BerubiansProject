d69db9908d1ed11fed51e2caed84f7836e9f49aa
Your Name <jonathan.r.gilbert@gmail.com>
2018-04-06 14:18:56 -0700

    added JS inputs for all 8 types of shows (currently only MUSICAL visibly changes to the user, as I don't yet have art assets for the other 7)

--- assets/js/swap.js
+++ assets/js/swap.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
 
         //  SWITCHBOARD creates a complex banner image by targeting the BANNER, PING, and EXTRA divs (by ID)
         //  SWITCHBOARD then assigns these divs CSS classes which define background properties 
-        //      BANNER appears below PING and PING appears below EXTRA 
+        //      BANNER appears below PING and PING appears below EXTRA (EXTRA > PING > BANNER) 
         //      All 3 divs are 150px x 100% (MIN-WIDTH: 380px), so the images will overlap correctly, and appear to be 1 single image
         //      PING resets the function ON.CLICK (hence the name PING) 
 
         //  generate a number between 0 and 9 each time SWITCHBOARD is called
-        let switchboard = Math.floor(Math.random() * 10);
+        let switchboard = Math.floor(Math.random() * 9);
         console.log(switchboard);
 
         //  prevent overlap by removing existing CSS
@@ -22,12 +22,64 @@ $(document).ready(function () {
         if (switchboard == 0) {
             $("#banner").addClass("bannerMusic");
             $("#ping").addClass("pingMusic");
-            $("#ping").addClass("extraMusic");
+            $("#extra").addClass("extraMusic");
+        }
+        //  swithboard = 1 | drama
+        if (switchboard == 1) {
+            $("#banner").addClass("bannerDrama");
+            $("#ping").addClass("pingDrama");
+            $("#extra").addClass("extraDrama");
+        }
+        //  swithboard = 2 | comedy
+        if (switchboard == 2) {
+            $("#banner").addClass("bannerComedy");
+            $("#ping").addClass("pingComedy");
+            $("#extra").addClass("extraComedy");
+        }
+        //  swithboard = 3 | horror
+        if (switchboard == 3) {
+            $("#banner").addClass("bannerHorror");
+            $("#ping").addClass("pingHorror");
+            $("#extra").addClass("extraHorror");
+        }
+        //  swithboard = 4 | action
+        if (switchboard == 4) {
+            $("#banner").addClass("bannerAction");
+            $("#ping").addClass("pingAction");
+            $("#extra").addClass("extraAction");
+        }
+        //  swithboard = 5 | scifi
+        if (switchboard == 5) {
+            $("#banner").addClass("bannerScifi");
+            $("#ping").addClass("pingScifi");
+            $("#extra").addClass("extraScifi");
+        }
+        //  swithboard = 6 | fantasy
+        if (switchboard == 6) {
+            $("#banner").addClass("bannerFantasy");
+            $("#ping").addClass("pingFantasy");
+            $("#extra").addClass("extraFantasy");
+        }
+        //  swithboard = 7 | action
+        if (switchboard == 7) {
+            $("#banner").addClass("bannerAction");
+            $("#ping").addClass("pingAction");
+            $("#extra").addClass("extraAction");
+        }
+        //  swithboard = 8 | standup
+        if (switchboard == 8) {
+            $("#banner").addClass("bannerStandup");
+            $("#ping").addClass("pingStandup");
+            $("#extra").addClass("extraStandup");
         }
 
     }
 
     // call the function when the page launches
     switchboard();
+
+    $("#ping").click(function () {
+        switchboard();
+    });
 
 });

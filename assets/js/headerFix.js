9267bf1ce9d381d4a147b571c5038e785650cab1
Your Name <jonathan.r.gilbert@gmail.com>
2018-04-09 11:08:56 -0700

    improved footer link spacing/centering. removed DIRECTIONS link from footer and repalced it with a RENT link. removed HOME ICON and replaced it with a NAVIGATION ICON (to replace the removed DIRECTIONS link). determining how to handle HOME page requests now that the HOME ICON has been replaced.

--- assets/js/headerFix.js
+++ assets/js/headerFix.js
@@ -1,5 +1,7 @@
+// pins the LINKS div to the top of the screen after the user scrolls down a certain distance
+// un-pins it if they scroll back above the scroll limit
 $(window).scroll(function () {
-    var sticky = $('.sticky'),
+    const sticky = $('.sticky'),
         scroll = $(window).scrollTop();
 
     if (scroll >= 100) sticky.addClass('fixed');

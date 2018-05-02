2c87a895d48fdf4b35b11769577cca1c01783986
Your Name <jonathan.r.gilbert@gmail.com>
2018-05-02 10:58:43 -0700

    trying a no-JS favicon solution to make heroku accept the icon

--- public/assets/js/swap.js
+++ public/assets/js/swap.js
@@ -57,7 +57,3 @@ $(document).ready(function () {
     });
 
 });
-
-var favicon = require('serve-favicon');
-var path = require("path"); 
-app.use(favicon(path.join(__dirname,'public', 'assets','images','logo.png')));
